944ade229103a1a186a20710e743c5375617d815
gaddams <sgaddam2015@kellogg.northwestern.edu>
2014-05-24 23:26:02 -0500

    wall route implemented

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -15,7 +15,7 @@ class PhotosController < ApplicationController
     @photo = Photo.new
     @photo.image_url = params[:image_url]
     @photo.caption = params[:caption]
-    @photo.user_id = params[:user_id]
+    @photo.user_id = current_user.id
 
     if @photo.save
       redirect_to "/photos", :notice => "Photo created successfully."
@@ -26,6 +26,7 @@ class PhotosController < ApplicationController
 
   def edit
     @photo = Photo.find(params[:id])
+
   end
 
   def update
@@ -49,4 +50,13 @@ class PhotosController < ApplicationController
 
     redirect_to "/photos", :notice => "Photo deleted."
   end
+
+  def wall
+    @photos = Photo.where(:user_id => current_user.id)
+
+  end
+
+  def user_favorites
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -50,6 +50,12 @@ Rails.application.routes.draw do
 
   # DELETE
   get('/delete_photo/:id', { :controller => 'photos', :action => 'destroy' })
+
+  #WALL
+  get('/wall', { :controller => 'photos', :action => 'wall' })
+
+  #FAVORITES
+  get('/favorites/current_user.id', { :controller => 'photos', :action => 'user_favorites' })
   #------------------------------
 
   # The priority is based upon order of creation: first created -> highest priority.
